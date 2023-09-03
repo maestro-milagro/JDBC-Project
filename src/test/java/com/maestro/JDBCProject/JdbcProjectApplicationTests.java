1cfe005e28123b34086a51754c69ba7e45793884
Sergey Korchagin <millagros77@gmail.com>
2023-09-03 22:37:17 +0300

    init

---
+++ src/test/java/com/maestro/JDBCProject/JdbcProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maestro.JDBCProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
